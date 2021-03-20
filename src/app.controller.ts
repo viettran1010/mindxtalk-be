8ec12eb1bb9499b7061c17c580eb5625e2e12525
zippochoiloto <theanhptit94@gmail.com>
2021-03-20 17:39:39 +0700

    enable cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guards';
+import { UsersService } from './user/user.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private userService: UsersService) {}
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  
+  @Post('auth/register')
+  async register(@Body() req: any) {
+    await this.userService.register(req)
+  }
+
+
 }
