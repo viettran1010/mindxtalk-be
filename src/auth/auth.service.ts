96511b1ea05918bc06399e6f52c08673e41f2cda
Viet <48788462+viettran1010@users.noreply.github.com>
2021-03-20 20:11:49 +0700

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
     const payload = { username: user.username, sub: user.userId };
     return {
       access_token: this.jwtService.sign(payload),
+      test: "test"
     };
   }
 }
