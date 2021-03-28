c503541dc6dcf39070e6ffa3d69ddf32e9fbcc0e
Viet <48788462+viettran1010@users.noreply.github.com>
2021-03-28 14:58:14 +0700

    add swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,6 +9,16 @@ async function bootstrap() {
   app.use(helmet());
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe());
+  
+  const config = new DocumentBuilder() //config for swagger
+    .setTitle('Mindxtalk OpenAPI')
+    .setDescription('This document provides details on how to call APIs')
+    .setVersion('1.0')
+    .addTag('swagger')
+    .build();
+  const document = SwaggerModule.createDocument(app, config); // create swagger
+  SwaggerModule.setup('api', app, document); //setup swagger
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
